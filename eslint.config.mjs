d098bc642124a8a2d6c3b0a3173e040079b77419
funayu <funayu0908@gmail.com>
2024-08-22 17:23:47 +0900

    prettierとESLintの競合を防ぐ設定を追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import jestPlugin from "eslint-plugin-jest"
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import jestPlugin from 'eslint-plugin-jest';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  {languageOptions: { globals:  {...globals.browser, ...globals.node }}},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   // Jest用の設定を追加
   {
-    files: ["tests/**/*"], // テストファイルのパス
+    files: ['tests/**/*'], // テストファイルのパス
     plugins: {
       jest: jestPlugin,
     },
@@ -18,4 +19,6 @@ export default [
       ...jestPlugin.configs.recommended.rules, // 推奨ルールを適用
     },
   },
+  // Prettierとの競合を防ぐ設定
+  prettierConfig,
 ];
