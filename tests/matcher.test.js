cfd7f609a64ca8c848200ec115a0c96c4fe0d496
funayu <funayu0908@gmail.com>
2024-08-30 17:46:12 +0900

    数値のテスト

--- tests/matcher.test.js
+++ tests/matcher.test.js
@@ -37,3 +37,22 @@ test('zero', () => {
   expect(z).not.toBeTruthy(); // 真であるか の否定
   expect(z).toBeFalsy(); // 偽であるか
 });
+
+// 数値
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // 数値に対してはtoBeとtoEqualは同じ動作をする（オブジェクトや配列を比較する場合はtoEqualを使用する）
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
+// 浮動小数点の値が一致するかの確認
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  expect(value).toBe(0.3);
+  // expect(value).toBeCloseTo(0.3);
+});
